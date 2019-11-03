6cde998ab11177e9ccc380ab35c1d90f8be07205
nader.126 <nader.126@hotmail.com>
2019-11-03 02:48:10 +0200

    add new projects

---
+++ InventoryService/InventoryServiceHost.cs
@@ -0,0 +1,11 @@
+﻿using Gigya.Microdot.Orleans.Ninject.Host;
+using Gigya.Microdot.Ninject;
+using Gigya.Microdot.Logging.NLog;
+
+namespace InventoryService
+{
+    public class InventoryServiceHost : MicrodotOrleansServiceHost
+    {
+        public override ILoggingModule GetLoggingModule() => new NLogModule();
+    }
+}
